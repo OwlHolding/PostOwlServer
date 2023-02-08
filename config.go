c5fa8a1e5a0f74446fd134b6b5514c6bc9a1f6c7
romantihiy <romantihomirov1@gmail.com>
2023-02-08 09:00:34 +0300

    Fixed config mistake

---
+++ config.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type ServerConfig struct {
+	Token           string
+	Url             string
+	Port            string
+	MaxBotConns     int
+	ChansPerUser    int
+	CertFile        string
+	KeyFile         string
+	RedisUrl        string
+	MlServers       []string
+	SqlUser         string
+	SqlPass         string
+	MaxSqlConns     int
+	MaxSqlIdleConns int
+	AdminChatID     int64
+}
+
+func GenerateConfig(path string) {
+	config, _ := json.Marshal(ServerConfig{})
+	err := os.WriteFile(path, config, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func LoadConfig(path string) ServerConfig {
+	byte_config, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var config ServerConfig
+	err = json.Unmarshal(byte_config, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
+func LoadConfigFromEnv(variable string) ServerConfig {
+	value, exists := os.LookupEnv(variable)
+	if !exists {
+		log.Fatalf("Variable %s does not exist", variable)
+	}
+	return LoadConfig(value)
+}
